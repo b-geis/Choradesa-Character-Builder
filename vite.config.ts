9eb3142df084d4912b02cb1d54f9db52631fd9be
b-geis <bgeiser396@gmail.com>
2025-10-19 16:52:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-base: '/choradesa-character-builder/',
-plugins: [react()],
+  base: '/Choradesa-Character-Builder/',
+  plugins: [react()],
 });
+
+
